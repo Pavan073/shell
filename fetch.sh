de6ad67a0732b65d719de2309d90e7ab7016f96a
Pavan073 <50820864+Pavan073@users.noreply.github.com>
2019-05-22 12:47:14 +0530

    update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sh /home/ec2-user/unix/read.sh $1 | jq '.AccessKey.SecretAccessKey'| sed 's/"//g'  > /home/ec2-user/unix/secretkey.txt
-sh /home/ec2-user/unix/read.sh $1 | jq '.AccessKey.AccessKeyId'| sed 's/"//g'  > /home/ec2-user/unix/accesskey.txt
+sh /home/ec2-user/read.sh $1 | jq '.AccessKey.SecretAccessKey'| sed 's/"//g'  > /home/ec2-user/unix/secretkey.txt
+sh /home/ec2-user/read.sh $1 | jq '.AccessKey.AccessKeyId'| sed 's/"//g'  > /home/ec2-user/unix/accesskey.txt
